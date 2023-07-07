012188be2bc964eee764ea000e1afb9e57b2207c
Wojciech Świderski <wojciechiderski@gmail.com>
2023-07-07 13:37:55 +0200

    Done project commit

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,324 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<BigInteger> buff = new List<BigInteger>();
+        List<byte> buffor = new List<byte>();
+        List<byte> bufforCompare = new List<byte>();
+        List<byte> checkEncrypt = new List<byte>();
+        List<byte> checkDecrypt = new List<byte>();
+        List<byte[]> checkBlocks = new List<byte[]>();
+        List<byte[]> resultToByteArrayCheck = new List<byte[]>();
+        List<byte[]> resultToByteArrayCheckDECRYPT = new List<byte[]>();
+        List<byte[]> checkChunkz = new List<byte[]>();
+
+        public bool isFile = false;
+        public int keySize = 256;
+        //BigInteger[] buffer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+             
+           RSA log = new RSA(keySize);
+           log.GenerateKeys();
+           nPrivate.Text = log.N.ToString();
+           nPublic.Text = log.N.ToString();
+           ePrivate.Text = log.E.ToString();
+           dPublic.Text = log.D.ToString();
+            
+
+          
+        }
+        //enkrypcja
+        private void encrypt_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            buff.Clear();
+            string nKey = nPublic.Text;
+            string eKey = ePrivate.Text;
+            BigInteger BiPublicN = BigInteger.Parse(nKey);
+            BigInteger BiPrivateE = BigInteger.Parse(eKey);
+
+            RSA log = new RSA(keySize);
+
+            // new
+            DecryptTextBox.Text = "";
+            byte[] data;
+            BigInteger result = new BigInteger(0);
+            if(isFile)
+            {
+                data = buffor.ToArray();
+            }
+            else
+            {
+                data = Encoding.UTF8.GetBytes(EncryptTextBox.Text);
+            }
+            //data = Encoding.UTF8.GetBytes(EncryptTextBox.Text);
+            data = log.zeroPadding(data);
+
+            string helper = "";
+            for(int i =0; i < data.Length / 16; i++)
+            {
+                byte[] block = log.getDataFromBlock(data, i);
+                checkBlocks.Add(block);
+                BigInteger toCipher = new BigInteger(block);
+                result = log.Encrypt(toCipher, BiPrivateE, BiPublicN);
+                resultToByteArrayCheck.Add(result.ToByteArray());
+                checkEncrypt.AddRange(result.ToByteArray());
+                buff.Add(result);
+                helper += result.ToString();
+                helper += 0x255;
+            }
+            log.filesWrite("buff.txt", buff);
+            DecryptTextBox.Text = helper;
+            //byte[] byt = StringToByteArray(helper);
+        }
+
+        //dekrypcja
+        private void decrypt_btn_Click(object sender, RoutedEventArgs e)
+        {
+            buffor.Clear();
+            EncryptTextBox.Text = ""; 
+            RSA log = new RSA(keySize);
+
+            if(buff.Count <= 0)
+            {
+                buff = log.StringToBigIntConversion(log.filesRead("buff.txt"));
+            }
+
+            BigInteger cipher;
+
+            // get n public and d public keys and convert it into BigInteger
+            string nKey = nPrivate.Text;
+            string dKey = dPublic.Text;
+            BigInteger BiPrivateN = BigInteger.Parse(nKey);
+            BigInteger BiPublicD = BigInteger.Parse(dKey);
+
+            BigInteger decryptedCipher;
+            string helper = "";
+            for (int j = 0; j<buff.Count; j++)
+            {   
+                cipher = buff[j];
+                checkDecrypt.AddRange(buff[j].ToByteArray());
+                decryptedCipher = log.Decrypt(cipher, BiPublicD, BiPrivateN);
+                resultToByteArrayCheckDECRYPT.Add(decryptedCipher.ToByteArray());
+                //byte[] bytes = Convert.FromHexString(decryptedCipher.ToString().Replace("-", ""));
+                byte[] bytes = decryptedCipher.ToByteArray();
+                    bool flag = false;
+                    if (bytes.Length < 17)
+                    {
+                        bytes = log.zeroPadding(bytes);
+                        byte[] bytes2 = new byte[17];
+                        for (int i = 0; i < bytes.Length; i++)
+                        {
+                            bytes2[i] = bytes[i];
+                        }
+                        bytes2[16] = 0x00;
+                        bytes = bytes2;
+                        flag = true;
+                    }
+                    
+                    byte[] chunk;
+                    for (int i = 0; i < bytes.Length / 17; i++)
+                    {
+                        chunk = log.readDataFromBlock(bytes, i);
+                        checkChunkz.Add(chunk);
+                    if (flag)
+                        {
+                            chunk = log.removeAddedZeros(chunk);
+                        }
+                    //buffor.AddRange(chunk);
+                    //EncryptTextBox.Text += Encoding.UTF8.GetString(chunk);
+                    string converting = BitConverter.ToString(chunk).Replace("-","");
+                    helper += converting;
+                }             
+            }
+            byte[] readText = Convert.FromHexString(helper);
+            readText = log.removeAddedZeros(readText);
+            buffor.AddRange(readText);
+            EncryptTextBox.Text += Encoding.UTF8.GetString(readText);
+        }
+        static string BytesToStringConverted(byte[] bytes)
+        {
+            using (var stream = new MemoryStream(bytes))
+            {
+                using (var streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+
+        public static string ConvertHex(System.String hexString)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    System.String hs = string.Empty;
+
+                    hs = hexString.Substring(i, 2);
+                    uint decval = System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+
+                }
+
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
+        }
+
+        public static byte[] StringToByteArray(System.String hex)
+        {
+            int NumberChars = hex.Length / 2;
+            byte[] bytes = new byte[NumberChars];
+            using (var sr = new StringReader(hex))
+            {
+                for (int i = 0; i < NumberChars; i++)
+                    bytes[i] =
+                      Convert.ToByte(new string(new char[2] { (char)sr.Read(), (char)sr.Read() }), 16);
+            }
+            return bytes;
+        }
+
+        private void SaveToFileEncryptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i=0; i<buffor.Count; i++)
+            {
+                if(bufforCompare.Count> 0)
+                {
+                    if (buffor[i] != bufforCompare[i])
+                    {
+                        byte error1 = buffor[i];
+                        byte error2 = bufforCompare[i];
+                        return;
+                    }
+                }
+            }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                byte[] bytes = buffor.ToArray();
+
+                //byte[] bytes = Encoding.UTF8.GetBytes(EncryptTextBox.Text);
+                File.WriteAllBytes(saveFileDialog.FileName, bytes);
+
+            }
+        }
+
+        private void SaveDecryptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(DecryptTextBox.Text);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllBytes(saveFileDialog.FileName, bytes);
+            }
+        }
+
+        /*
+         * 
+         * Autorzy: Wojciech Świderski 242551, Mateusz Rybicki 242518
+         * Program: Implementacja RSA w C#
+         * Przedmiot: Podstawy kryptografii
+         * Rok: 2022/23, semestr 4
+         * 
+         */
+
+
+        private void LoadEncryptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                byte[] cos = File.ReadAllBytes(ofd.FileName);
+                buffor.AddRange(cos);
+                bufforCompare.AddRange(cos);
+                EncryptTextBox.Text = Encoding.UTF8.GetString(cos);
+            }
+        }
+
+        private void LoadDecryptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                byte[] cos = File.ReadAllBytes(ofd.FileName);
+                DecryptTextBox.Text = BitConverter.ToString(cos).Replace("-", "");
+                //DecryptTextBox.Text = Encoding.UTF8.GetString(cos);
+            }
+        }
+
+
+
+        //wybor dlugosci klucza
+        private void radio_128bit_Checked(object sender, RoutedEventArgs e)
+        {
+            keySize = 1024;
+        }
+
+        private void radio256bit_Checked(object sender, RoutedEventArgs e)
+        {
+            keySize = 256;
+        }
+
+        private void radio_512bit_Checked(object sender, RoutedEventArgs e)
+        {
+            keySize = 512;
+        }
+        //wybor czy plik czy text
+        private void text_radio_btn_Checked(object sender, RoutedEventArgs e)
+        {
+
+            isFile = false;
+        }
+
+        private void file_radio_btn_Checked(object sender, RoutedEventArgs e)
+        {
+            buffor.Clear();
+            isFile = true;
+        }
+    }
+
+
+}
---
+++ WpfApp3/WpfApp3/RSA.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * Autorzy: Wojciech Świderski 242551, Mateusz Rybicki 242518
+ * Program: Implementacja RSA w C#
+ * Przedmiot: Podstawy kryptografii
+ * Rok: 2022/23, semestr 4
+ * 
+ */
+
+namespace WpfApp3
+{
+    public class RSA
+    {
+        
+        private BigInteger _p;
+        private BigInteger _q;
+        private BigInteger _n;
+        private BigInteger _Euler;
+        private BigInteger _e = 65537;
+        private BigInteger _d;
+        private int bitLength;
+
+        public BigInteger N
+        {
+            get { return _n; }
+        }
+
+        public BigInteger E
+        {
+            get { return _e; }
+        }
+
+        public BigInteger D
+        {
+            get { return _d; }
+        }
+
+        public BigInteger Euler
+        {
+            get { return _Euler; }
+        }
+
+        public RSA(int bitLength)
+        {
+            this.bitLength = bitLength;
+        }
+
+        public void GenerateKeys()
+        {
+            Random random = new Random();
+            _q = GenerateProbablePrime(bitLength, random);
+            _p = GenerateProbablePrime(bitLength, random);
+            _n = _p * _q;
+            _Euler = (_p - BigInteger.One) * (_q - BigInteger.One);
+            _d = EuclideanAlgorithmExtended(_e, _Euler);
+        }
+        //enkrypcja
+        public BigInteger Encrypt(BigInteger message, BigInteger e, BigInteger n)
+        {
+            return BigInteger.ModPow(message, e, n);
+        }
+        //dekrypcja
+        public BigInteger Decrypt(BigInteger cipher, BigInteger d, BigInteger n)
+        {
+            return BigInteger.ModPow(cipher, d, n);
+
+        }
+        //po nazwie chyba wiadomo co :>
+        public static bool IsProbablePrime(BigInteger n, int k)
+        {
+            if (n <= 1 || n == 4)
+                return false;
+            if (n <= 3)
+                return true;
+
+            int s = 0;
+            BigInteger d = n - 1;
+            while (d % 2 == 0)
+            {
+                s++;
+                d /= 2;
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                Random random = new Random();
+                BigInteger a = RandomInRange(2, n - 2, random);
+                BigInteger x = BigInteger.ModPow(a, d, n);
+
+                if (x == 1 || x == n - 1)
+                    continue;
+
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, n);
+                    if (x == 1)
+                        return false;
+                    if (x == n - 1)
+                        break;
+                }
+
+                if (x != n - 1)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static BigInteger RandomInRange(BigInteger min, BigInteger max, Random random)
+        {
+            byte[] maxBytes = max.ToByteArray();
+            byte[] minBytes = min.ToByteArray();
+
+         
+
+            int maxLength = maxBytes.Length;
+            byte[] randomBytes = new byte[maxLength];
+
+            random.NextBytes(randomBytes);
+
+            BigInteger randomNumber = new BigInteger(randomBytes);
+
+            // ensure the random number is within the range [min, max]
+            BigInteger diff = max - min;
+            BigInteger randomValueInRange = (randomNumber % diff) + min;
+
+            return randomValueInRange;
+        }
+
+        //generowanie liczb pierwszych
+        public static BigInteger GenerateProbablePrime(int bitLength, Random random)
+        {
+            while (true)
+            {
+                BigInteger prime = GenerateRandom(bitLength, random);
+                if (prime.IsEven)
+                {
+                    prime += 1;
+                }
+                if (IsProbablePrime(prime,10))
+                {
+                    return prime;
+                }
+            }
+        }
+        public static BigInteger GenerateRandom(int bitLength, Random random)
+        {
+            byte[] bytes = new byte[bitLength / 8 + 1];
+            random.NextBytes(bytes);
+            bytes[bytes.Length - 1] &= 0x7F; // ensure highest bit is 0 to make positive
+            return new BigInteger(bytes);
+        }
+        //rozszerzony algorytm euklidesa
+        public static BigInteger EuclideanAlgorithmExtended(BigInteger a, BigInteger b)
+        {
+            BigInteger u = BigInteger.One, w = a, x = BigInteger.Zero, z = b, q, tmp;
+            while (w != BigInteger.Zero)
+            {
+                if (w < z)
+                {
+                    tmp = u;
+                    u = x;
+                    x = tmp;
+
+                    tmp = w;
+                    w = z;
+                    z = tmp;
+                }
+                q = w / z;
+                u -= q * x;
+                w -= q * z;
+            }
+            if (z == BigInteger.One)
+            {
+                if (x < BigInteger.Zero)
+                {
+                    x += b;
+                }
+                return x;
+            }
+            return x;
+        }
+
+        //nwd jest w biginteger ale jakos dziwnie dzialał
+        public static BigInteger EuclideanAlgorithm(BigInteger a, BigInteger b)
+        {
+            BigInteger tmp;
+            while (b != BigInteger.Zero)
+            {
+                tmp = a % b;
+                a = b;
+                b = tmp;
+            }
+            return a;
+        }
+
+        public static bool IsPrime(BigInteger a, BigInteger b)
+        {
+            return EuclideanAlgorithm(a, b) == BigInteger.One;
+        }
+
+        public static BigInteger GenerateCoprimeNumber(BigInteger max)
+        {
+            BigInteger tmp = max / 3;
+            while (!IsPrime(max, tmp))
+            {
+                tmp++;
+            }
+            return tmp;
+        }
+
+
+
+        // zero padding to 16 bytes 
+        public byte[] zeroPadding(byte[] array)
+        {
+            int pom = (array.Length) % 16;
+            int lastN = pom;
+
+            if (lastN == 0)
+            {
+                return array;
+            }
+
+            int temp = array.Length;
+            int addedBytes = 16 - lastN;
+            byte[] completedArray = new byte[addedBytes + temp];
+
+            System.Array.Copy(array, 0, completedArray, 0, temp);
+
+            completedArray[temp] = (byte)0xFF; // 0xFF is maximum size of bytes
+
+            for (int i = temp + 1; i < (addedBytes + array.Length); i++)
+            {
+                completedArray[i] = (byte)0x00;
+
+            }
+
+            return completedArray;
+        }
+        // remove adding zeros
+        // array to nasza ta tablica
+        public byte[] removeAddedZeros(byte[] array)
+        {
+            int digit0 = 0;
+
+
+            int len = array.Length - 1;
+            int i = len;
+            int condition = array.Length - 16;
+
+            //idziemy jakby od przedostatniego bajtu bo na ostatnim jest ta liczba tych 0 
+            while (i >= condition)
+            {
+
+                if (array[i] == 0)
+                {
+                    digit0 += 1; // inkremetnacja
+                }
+                else if (array[i] != 0)
+                {
+                    break;
+                }
+                i--; // deikrementacja
+
+            }
+
+            if (digit0 != 0 || array[len] == (byte)0xFF)
+            {
+                digit0 = digit0 + 1;
+            }
+
+            byte[] firstForm = new byte[len - digit0 + 1];
+
+
+            for (int j = 0; j < len - digit0 + 1; j++)
+            {
+                firstForm[j] = array[j];
+
+
+            }
+
+            return firstForm;
+        }
+
+        //dajemy dane do bloków i dodajemy 1 zeby biginteger tego nie usunał
+        public byte[] getDataFromBlock(byte[] array, int digit)
+        {
+            byte[] new_tab = new byte[17];
+            new_tab[16] = 1;
+            int i = 0;
+
+            while (i < 16)
+            {
+                new_tab[i] = array[16 * digit + i];
+                i += 1;
+            }
+
+            return new_tab;
+        }
+        // odczytujemy dane z bloków, plus odejmujemy nasza 1
+        public byte[] readDataFromBlock(byte[] array, int digit)
+        {
+            byte[] new_tab = new byte[16];
+            int i = 0;
+
+            while (i < 16)
+            {
+                new_tab[i] = array[17 * digit + i];
+                i += 1;
+            }
+
+            return new_tab;
+        }
+
+        public void filesWrite(string path, List<BigInteger> buff)
+        {
+            //if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    for(int i = 0; i < buff.Count; i++)
+                    {
+                        sw.WriteLine(buff[i].ToString());
+                    }
+                }
+            }
+        }
+
+        public void filesWrite(string path, string messege)
+        {
+             // Create a file to write to.
+             using (StreamWriter sw = File.CreateText(path))
+             {
+                 sw.WriteLine(messege);
+             }
+        }
+
+        public List<string> filesRead(string path)
+        {
+            List<string> list = new List<string>();
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    list.Add(s);
+                }
+            }
+            return list;
+        }
+
+        public List<BigInteger> StringToBigIntConversion(List<string> list)
+        {
+            List<BigInteger> cipher = new List<BigInteger>();
+            for(int i=0; i < list.Count; i++)
+            {
+                cipher.Add(BigInteger.Parse(list[i]));
+            }
+            return cipher;
+        }
+    }
+}
